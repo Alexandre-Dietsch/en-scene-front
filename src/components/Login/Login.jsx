f8104b8e684e41ed8ba55fc15d112b0308d07a5e
Alexandre-Dietsch <dietschalex@gmail.com>
2021-02-03 23:37:13 +0100

    login completed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import pictures from './loginPictures';
+import ListOfPictures from './ListOfPictures';
 
 export default function Login() {
   return(
     <div className='login-container'>
-      {
-        pictures.map(picture => (
-          <img key={picture.id} src={picture.picture} alt='' className={picture.cName} />
-         ))
-      }
-      <button type='button' className='login-button'>
-        Se connecter
-      </button>
-      <span>
-        Vous n'avez pas de conmpte ? <strong>Inscivez-vous</strong>
-      </span>
+      <div className="login-content">
+        <div className="login-background" />
+        <ListOfPictures />
+        <button type='button' className='login-button'>
+          Se connecter
+        </button>
+        <span>
+          Vous n'avez pas de conmpte ? <strong>Inscivez-vous</strong>
+        </span>
+      </div>
     </div>
   );
 }
