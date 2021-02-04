7ed8ef38c47f3a31b7ceafc44e09e482000fa74d
Alexandre-Dietsch <dietschalex@gmail.com>
2021-02-04 22:54:47 +0100

    profil page added

---
+++ src/components/Artists/ArtistProfil.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import MobileMenu from '../Menu/MobileMenu';
+
+export default function ArtistProfil() {
+  const [artistProfil, setArtistProfil] = useState([]);
+  const { idartist } = useParams();
+
+  useEffect(() => {
+    const fetchArtistProfil = async (req, res) => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/user/${idartist}`);
+        setArtistProfil(response.data);
+      } catch(error) {
+        console.log(error.message)
+      }
+    }
+    fetchArtistProfil();
+  })
+
+  return(
+    <div className="artist-container">
+      {
+        artistProfil && 
+        artistProfil.map(artist => (
+          <div className="artist-profil">
+            <img src={artist.user_picture} alt='' />
+            <div className="artist-biography">
+              <p>{artist.biography}</p>
+              <span>Découvrir {artist.pseudo}</span>
+            </div>
+          </div>
+        ))
+      }
+
+    <MobileMenu />
+    </div>
+  );
+};
